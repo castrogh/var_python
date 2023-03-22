f6f2f0cfef88d777cf20ecfc5b07927b409a8a2c
Gabriel Castro <ghscastro@hotmail.com>
2023-03-22 14:00:04 -0300

    Adição da explicação do laço WHILE.

--- laco_WHILE.py
+++ laco_WHILE.py
@@ -8,3 +8,7 @@ else:
         print("\t" + str(numero) + " x " + str(cont) + " = " + str(numero*cont)) #O \t dá um espaço à direita no texto
         cont = cont+1
     print("Esta é a tabuada do número " + str(numero))
+
+    #O laço de repetição while serve para que um mesmo trecho de código, ou seja, uma terefa, seja repetido até que uma determinada condição seja satisfeita
+    #Para exemplificar este funcionamento, foi desenvolvido um código que simula uma tabuada convencional
+    #O código para de ser executado assim que a variável cont alcança o valor igual a 10.
